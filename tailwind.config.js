cbb6b213d817e2c2a726e22c8ba919abac7a52d8
vickyrathode <114683805+vickyrathode@users.noreply.github.com>
2025-01-27 12:49:27 +0530

    error of tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Add paths to all your files
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
