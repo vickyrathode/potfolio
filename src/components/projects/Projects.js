fe1aa91e866d9852b7b7b2ab55499500279e0546
vickyrathode <114683805+vickyrathode@users.noreply.github.com>
2025-01-27 13:19:19 +0530

    project name

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -204,7 +204,7 @@ const Projects = () => {
   return (
     <section id="projects" className="py-16 bg-gray-100">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-6">Portfolio</h2>
+        <h2 className="text-3xl font-bold mb-6">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {currentProjects.map((project) => (
             <div
